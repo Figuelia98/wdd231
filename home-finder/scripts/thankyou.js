bd0b0cab5543f0015c22cd9ec4961fab81284d00
Figuelia98 <figueliayasin98@gmail.com>
2025-02-19 00:20:57 +0300

    Post 08

--- home-finder/scripts/thankyou.js
+++ home-finder/scripts/thankyou.js
@@ -9,22 +9,5 @@ document.getElementById('first_name').textContent = params.get('firstName');
 document.getElementById('last_name').textContent = params.get('lastName');
 document.getElementById('email').textContent = params.get('email');
 document.getElementById('mobile').textContent = params.get('phone');
-document.getElementById('city').textContent = params.get('name');
+document.getElementById('city').textContent = params.get('city');
 document.getElementById('timestamp').textContent = params.get('timestamp');
-
-const mode = document.querySelector(".mode")
-mode.addEventListener("click", ()=>{
-//alert( document.querySelector("body").style.backgroundColor);
-if(document.querySelector("body").style.backgroundColor == "" ||document.querySelector("body").style.backgroundColor == "white"){
-document.querySelector("body").style.backgroundColor = "#373F51";
-document.querySelector(".h2").style.color = "#2C535A";
-document.querySelector(".h22").style.color = "#2C535A";
-}
-else{
-document.querySelector("body").style.backgroundColor = "white";
-document.querySelector(".h2").style.color = "#373F51";
-document.querySelector(".h22").style.color = "#373F51";
-}
-
-
-});
